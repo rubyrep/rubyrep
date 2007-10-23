fe7bc53ad4ef8edbf4cacc11d05cafc4c6e20ee3
Arndt Lehmann <mail@arndtlehmann.com>
2007-10-23 12:20:04 +0000

    renamed Rubyrep module to RR

--- config/hoe.rb
+++ config/hoe.rb
@@ -31,7 +31,7 @@ end
 REV = nil 
 # UNCOMMENT IF REQUIRED: 
 # REV = `svn info`.each {|line| if line =~ /^Revision:/ then k,v = line.split(': '); break v.chomp; else next; end} rescue nil
-VERS = Rubyrep::VERSION::STRING + (REV ? ".#{REV}" : "")
+VERS = RR::VERSION::STRING + (REV ? ".#{REV}" : "")
 RDOC_OPTS = ['--quiet', '--title', 'rubyrep documentation',
     "--opname", "index.html",
     "--line-numbers", 
--- lib/rubyrep.rb
+++ lib/rubyrep.rb
@@ -1,5 +1,5 @@
 $:.unshift File.dirname(__FILE__)
 
-module Rubyrep
+module RR
   
 end
