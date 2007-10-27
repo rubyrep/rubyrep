c8330aeba612307c16d77fbe4e874f0eb7477f49
Arndt Lehmann <mail@arndtlehmann.com>
2007-10-27 15:09:05 +0000

    Code formatting

--- lib/rubyrep/initializer.rb
+++ lib/rubyrep/initializer.rb
@@ -1,16 +1,16 @@
 module RR
-  
+
   # The Configuration class holds the default configuration options for Rubyrep.
   # Configuration values are changed with the Initializer::run method.
   class Configuration
     # connection settings for the "left" database
     attr_accessor :left
-    
+
     # connection settings for the "right" database
     attr_accessor :right
 
   end
-  
+
   # The settings of the current deployment are passed to Rubyrep through the
   # Initializer::run method.
   # The method yields the a Configuration object for overwriting of the default
@@ -23,18 +23,18 @@ module RR
   class Initializer
 
     # Sets a new Configuration object
-    # Current configuration values are lost and replaced with the default 
+    # Current configuration values are lost and replaced with the default
     # settings.
     def self.reset
       @@configuration = Configuration.new
     end
     reset
-    
+
     # Returns the current Configuration object
     def self.configuration
       @@configuration
     end
-    
+
     # Yields the current Configuration object to enabl overwriting of
     # configuration values.
     # See class documentation for usage example.
