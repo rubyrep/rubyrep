ca6010858cab95721bef1f39ed6470cebccf3961
Arndt Lehmann <mail@arndtlehmann.com>
2007-11-12 22:18:05 +0000

    postgresql_extender code beautification

--- lib/rubyrep/connection_extenders/postgresql_extender.rb
+++ lib/rubyrep/connection_extenders/postgresql_extender.rb
@@ -74,41 +74,41 @@ module RR
           WHERE relname = '#{table}'
         end_sql
         if row.nil?
-        raise "table does not exist"
-      end
+          raise "table does not exist"
+        end
         
-      row = self.select_one(<<-end_sql)
-          SELECT cons.conkey 
-          FROM pg_class           rel
-          JOIN pg_constraint      cons ON (rel.oid = cons.conrelid)
-          WHERE cons.contype = 'p' AND rel.relname = '#{table}'          
-      end_sql
-      if row.nil?
-      return []
-    end
-    column_parray = row['conkey']
+        row = self.select_one(<<-end_sql)
+            SELECT cons.conkey 
+            FROM pg_class           rel
+            JOIN pg_constraint      cons ON (rel.oid = cons.conrelid)
+            WHERE cons.contype = 'p' AND rel.relname = '#{table}'          
+        end_sql
+        if row.nil?
+          return []
+        end
+        column_parray = row['conkey']
         
-    # Change a Postgres Array of attribute numbers 
-    # (returned in String form, e. g.: "{1,2}") into an array of Integers
-    column_ids = column_parray.sub(/^\{(.*)\}$/,'\1').split(',').map {|a| a.to_i}
-        
-    columns = {}
-    rows = self.select_all(<<-end_sql)
+        # Change a Postgres Array of attribute numbers 
+        # (returned in String form, e. g.: "{1,2}") into an array of Integers
+        column_ids = column_parray.sub(/^\{(.*)\}$/,'\1').split(',').map {|a| a.to_i}
+
+        columns = {}
+        rows = self.select_all(<<-end_sql)
           SELECT attnum, attname 
           FROM pg_class           rel
           JOIN pg_constraint      cons ON (rel.oid = cons.conrelid)
           JOIN pg_attribute       attr ON (rel.oid = attr.attrelid and attr.attnum = any (cons.conkey))
           WHERE cons.contype = 'p' AND rel.relname = '#{table}'
-    end_sql
-    sorted_columns = []
-    if not rows.nil?
-      rows.each() {|row| columns[row['attnum'].to_i] = row['attname']}
-      sorted_columns = column_ids.map {|column_id| columns[column_id]}
+        end_sql
+        sorted_columns = []
+        if not rows.nil?
+          rows.each() {|r| columns[r['attnum'].to_i] = r['attname']}
+          sorted_columns = column_ids.map {|column_id| columns[column_id]}
+        end
+        sorted_columns
+      end
+
     end
-    sorted_columns
   end
-      
-end
-end
 end
 
