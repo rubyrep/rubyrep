a56fee3c845571419c00d07f6928719f0437ea68
Arndt Lehmann <mail@arndtlehmann.com>
2007-12-18 09:21:03 +0000

    Dried up big_scan_spec.rb

--- sims/big_scan/big_scan_spec.rb
+++ sims/big_scan/big_scan_spec.rb
@@ -9,11 +9,9 @@ describe "Big Scan" do
   before(:each) do
   end
 
-  it "ProxiedTableScan should identify differences between big_scan tables correctly" do
-    puts 'Scanning table big_scan using ProxiedTableScan'
-    Initializer.configuration = deep_copy(proxied_config)
-    Initializer.configuration.proxy_options[:block_size] = 100
-    ensure_proxy
+  # Runs a scan of the big_scan table using the provided Scanner class
+  def run_scan(table_scan_class)
+    puts "\nScanning table big_scan using #{table_scan_class.name}"
     session = Session.new
     expected_result = {}
     expected_result[:conflict] = session.left.select_one( \
@@ -27,7 +25,7 @@ describe "Big Scan" do
     
     progress_bar = ProgressBar.new expected_result.values.inject {|sum, n| sum + n }
 
-    scan = ProxiedTableScan.new session, 'big_scan'
+    scan = table_scan_class.new session, 'big_scan'
     benchmark = Benchmark.measure {
       scan.run do |diff_type, row|
         progress_bar.step
@@ -38,32 +36,18 @@ describe "Big Scan" do
     
     received_result.should == expected_result
   end
+  
+  it "ProxiedTableScan should identify differences between big_scan tables correctly" do
+    Initializer.configuration = deep_copy(proxied_config)
+    Initializer.configuration.proxy_options[:block_size] = 100
+    ensure_proxy
+    
+    run_scan ProxiedTableScan
+  end
 
   it "DirectTableScan should identify differences between big_scan tables correctly" do
-    puts 'Scanning table big_scan using DirectTableScan'
     Initializer.configuration = standard_config
-    session = Session.new
-    expected_result = {}
-    expected_result[:conflict] = session.left.select_one( \
-        "select count(id) as count from big_scan where diff_type = 'conflict'")['count'].to_i
-    expected_result[:left] = session.left.select_one( \
-        "select count(id) as count from big_scan where diff_type = 'left'")['count'].to_i
-    expected_result[:right] = session.right.select_one( \
-        "select count(id) as count from big_scan where diff_type = 'right'")['count'].to_i
     
-    received_result = {:conflict => 0, :left => 0, :right => 0}
-    
-    progress_bar = ProgressBar.new expected_result.values.inject {|sum, n| sum + n }
-
-    scan = DirectTableScan.new session, 'big_scan'
-    benchmark = Benchmark.measure {
-      scan.run do |diff_type, |
-        progress_bar.step
-        received_result[diff_type] += 1
-      end
-    }
-    puts "  time required: #{benchmark}"
-    
-    received_result.should == expected_result
+    run_scan DirectTableScan
   end
 end
