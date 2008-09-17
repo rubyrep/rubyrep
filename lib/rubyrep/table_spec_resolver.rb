c04c0bb01d113043add81d76ff1b17a1178430de
Arndt Lehmann <mail@arndtlehmann.com>
2008-09-17 20:33:36 +0900

    Refactored TableSpecResolver#resolve to simplify the code.

--- lib/rubyrep/table_spec_resolver.rb
+++ lib/rubyrep/table_spec_resolver.rb
@@ -19,9 +19,8 @@ module RR
     # ScanRunner#get_options). Returns an array of table name pairs in Hash form. 
     # E. g. something like 
     # [{:left_table => 'my_table', :right_table => 'my_table_backup'}]
-    # Takes care that a table is only added once.
+    # Takes care that a table is only returned once.
     def resolve(table_specs)
-      already_resolved_tables = {}
       table_pairs = []
       table_specs.each do |table_spec|
         case table_spec
@@ -29,25 +28,35 @@ module RR
           table_spec = table_spec.sub(/^\/(.*)\/$/,'\1') # remove leading and trailing slash
           matching_tables = tables.grep(Regexp.new(table_spec, Regexp::IGNORECASE, 'U'))
           matching_tables.each do |table|
-            unless already_resolved_tables.include? table
-              table_pairs << {:left_table => table, :right_table => table}
-              already_resolved_tables[table] = true
-            end
+            table_pairs << {:left_table => table, :right_table => table}
           end
         when /.+,.+/ # matches e. g. 'users,users_backup'
           pair = table_spec.match(/(.*),(.*)/)[1..2].map { |str| str.strip }
-          unless already_resolved_tables.include? pair[0]
-            table_pairs << {:left_table  => pair[0], :right_table => pair[1]}
-            already_resolved_tables[pair[0]] = true
-          end
+          table_pairs << {:left_table  => pair[0], :right_table => pair[1]}
         else # everything else: just a normal table
-          unless already_resolved_tables.include? table_spec.strip
-            table_pairs << {:left_table => table_spec.strip, :right_table => table_spec.strip}
-            already_resolved_tables[table_spec.strip] = true
-          end
+          table_pairs << {:left_table => table_spec.strip, :right_table => table_spec.strip}
         end
       end
-      table_pairs
+      remove_duplicate_table_pairs(table_pairs)
     end
+
+    # Helper for #resolve
+    # Takes given table_pairs and removes all duplicates.
+    # Returns the result.
+    # Both the given and the returned table_pairs is an array of hashes with
+    # * :+left_table+: name of the left table
+    # * :+right_table+: name of the corresponding right table
+    def remove_duplicate_table_pairs(table_pairs)
+      processed_left_tables = {}
+      resulting_table_pairs = []
+      table_pairs.each do |table_pair|
+        unless processed_left_tables.include? table_pair[:left_table]
+          resulting_table_pairs << table_pair
+          processed_left_tables[table_pair[:left_table]] = true
+        end
+      end
+      resulting_table_pairs
+    end
+    private :remove_duplicate_table_pairs
   end
 end
