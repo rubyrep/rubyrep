d689ff71649fbced7c6e1ffcd91bed5ae2cc4de0
Arndt Lehmann <mail@arndtlehmann.com>
2009-01-29 01:20:06 +0900

    ScanDetailReport can now report differences either as full dumps, diffs or key columns only.

--- lib/rubyrep/scan_report_printers/scan_detail_reporter.rb
+++ lib/rubyrep/scan_report_printers/scan_detail_reporter.rb
@@ -9,10 +9,14 @@ module RR::ScanReportPrinters
     
     # Register ScanSummaryReporter with the given command line options.
     # (Command line format as specified by OptionParser#on.)
-    RR::ScanReportPrinters.register self, "-d", "--detailed",
-        "Print the number of differences of each table. E. g.",
-        "  left_table / right_table [differences]",
-        "followed by a full dump of the differences in YAML format"
+    RR::ScanReportPrinters.register self, "-d", "--detailed[=mode]",
+      "Print the number of differences of each table. E. g.",
+      "  left_table / right_table [differences]",
+      "followed by a full dump of the differences in YAML format.",
+      "The 'mode' argument determines how the row differences are printed:",
+      " * full shows the full records",
+      " * keys shows the primary key columns only",
+      " * diff shows the primary key and differing columsn only"
     
     # The current Session object
     attr_accessor :session
@@ -20,11 +24,30 @@ module RR::ScanReportPrinters
     # The temporary File receiving the differences
     attr_accessor :tmpfile
 
+    # Mode of reporting. Should be either
+    # * :+full+
+    # * :+keys+ or
+    # * :+diff+
+    attr_accessor :report_mode
+
+    # Array of names of the primary key columns of the table currently being
+    # scanned.
+    attr_accessor :primary_key_names
+
     # A scan run is to be started using this scan result printer.
     # +arg+ is the command line argument as yielded by OptionParser#on.
     def initialize(session, arg)
       super session, ""
       self.session = session
+
+      self.report_mode = case arg
+      when 'diff'
+        :diff
+      when 'keys'
+        :keys
+      else
+        :full
+      end
     end
     
     # A scan of the given 'left' table and corresponding 'right' table is executed.
@@ -34,6 +57,7 @@ module RR::ScanReportPrinters
       super left_table, right_table
 
     ensure
+      self.primary_key_names = nil
       if self.tmpfile
         self.tmpfile.close
         self.tmpfile.open
@@ -43,11 +67,38 @@ module RR::ScanReportPrinters
       end
     end
 
+    # Returns a cleaned row as per current +report_mode+.
+    # +row+ is either a column_name => value hash or an array of 2 such rows.
+    def clear_columns(row)
+      case report_mode
+      when :full
+        row
+      when :keys
+        row = row[0] if row.kind_of?(Array)
+        self.primary_key_names ||= session.left.primary_key_names(self.left_table)
+        row.reject {|column, value| !self.primary_key_names.include?(column)}
+      when :diff
+        self.primary_key_names ||= session.left.primary_key_names(self.left_table)
+        if row.kind_of?(Array)
+          new_row_array = [{}, {}]
+          row[0].each do |column, value|
+            if self.primary_key_names.include?(column) or value != row[1][column]
+              new_row_array[0][column] = row[0][column]
+              new_row_array[1][column] = row[1][column]
+            end
+          end
+          new_row_array
+        else
+          row
+        end
+      end
+    end
+
     # Each difference is handed to the printer as described in the format
     # as described e. g. in DirectTableScan#run
     def report_difference(type, row)
       self.tmpfile ||= Tempfile.new 'rubyrep_scan_details'
-      tmpfile.puts({type => row}.to_yaml)
+      tmpfile.puts({type => clear_columns(row)}.to_yaml)
       super type, row
     end
 
--- spec/scan_detail_reporter_spec.rb
+++ spec/scan_detail_reporter_spec.rb
@@ -4,6 +4,7 @@ include RR
 
 describe ScanReportPrinters::ScanDetailReporter do
   before(:each) do
+    Initializer.configuration = standard_config
     $stdout.should_receive(:puts).any_number_of_times
   end
 
@@ -17,11 +18,11 @@ describe ScanReportPrinters::ScanDetailReporter do
     ScanReportPrinters::ScanDetailReporter.new(:dummy_session, nil).session.should == :dummy_session
   end
   
-  it "scan should print the summary and details of the differences" do
+  it "scan should print the summary and the dump of the differences if mode = 'full'" do
     org_stdout = $stdout
     $stdout = StringIO.new
     begin
-      reporter = ScanReportPrinters::ScanDetailReporter.new(nil, nil)
+      reporter = ScanReportPrinters::ScanDetailReporter.new(nil, 'full')
       
       # set some existing scan result to ensure it gets reset before the next run
       reporter.scan_result = {:conflict => 0, :left => 0, :right => 1}
@@ -31,11 +32,87 @@ describe ScanReportPrinters::ScanDetailReporter do
         reporter.report_difference :left, :dummy_row
         reporter.report_difference :right, :dummy_row
       end
+      
+      # verify summary
       $stdout.string.should =~ /left_table \/ right_table [\.\s]*3\n/
-      $stdout.string.should =~ /:conflict.*dummy_row/
-      $stdout.string.should =~ /:left.*dummy_row/
-      $stdout.string.should =~ /:right.*dummy_row/
+
+      # verify dump
+      io = StringIO.new($stdout.string.gsub(/^.*left_table.*$/, ''))
+      dump_objects = []
+      YAML.load_documents(io) do |yl|
+        dump_objects << yl
+      end
+      dump_objects.should == [
+        {:conflict=>:dummy_row},
+        {:left=>:dummy_row},
+        {:right=>:dummy_row}
+      ]
     ensure 
+      $stdout = org_stdout
+    end
+  end
+
+  it "scan should print the summary and the keys of the differences if mode = 'keys'" do
+    org_stdout = $stdout
+    $stdout = StringIO.new
+    begin
+      session = Session.new
+      reporter = ScanReportPrinters::ScanDetailReporter.new(session, 'keys')
+
+      # set some existing scan result to ensure it gets reset before the next run
+      reporter.scan_result = {:conflict => 0, :left => 0, :right => 1}
+
+      reporter.scan('scanner_records', 'scanner_records') do
+        reporter.report_difference :conflict, [{'id' => 1, 'name' => 'bla'}, {'id' => 1, 'name' => 'blub'}]
+        reporter.report_difference :left, {'id' => 2, 'name' => 'bla'}
+        reporter.report_difference :right, {'id' => 3, 'name' => 'blub'}
+      end
+
+      io = StringIO.new($stdout.string.gsub(/^.*scanner_records.*$/, ''))
+      dump_objects = []
+      YAML.load_documents(io) do |yl|
+        dump_objects << yl
+      end
+      dump_objects.should == [
+        {:conflict=>{"id"=>1}},
+        {:left=>{"id"=>2}},
+        {:right=>{"id"=>3}}
+      ]
+    ensure
+      $stdout = org_stdout
+    end
+  end
+
+  it "scan should print the summary and the differing columns of the differences if mode = 'diff'" do
+    org_stdout = $stdout
+    $stdout = StringIO.new
+    begin
+      session = Session.new
+      reporter = ScanReportPrinters::ScanDetailReporter.new(session, 'diff')
+
+      # set some existing scan result to ensure it gets reset before the next run
+      reporter.scan_result = {:conflict => 0, :left => 0, :right => 1}
+
+      reporter.scan('scanner_records', 'scanner_records') do
+        reporter.report_difference :conflict, [
+          {'id' => 1, 'name' => 'bla', 'age' => 20},
+          {'id' => 1, 'name' => 'blub', 'age' => 20}
+        ]
+        reporter.report_difference :left, {'id' => 2, 'name' => 'bla'}
+        reporter.report_difference :right, {'id' => 3, 'name' => 'blub'}
+      end
+
+      io = StringIO.new($stdout.string.gsub(/^.*scanner_records.*$/, ''))
+      dump_objects = []
+      YAML.load_documents(io) do |yl|
+        dump_objects << yl
+      end
+      dump_objects.should == [
+        {:conflict=>[{'id' => 1, 'name' => 'bla'}, {'id' => 1, 'name' => 'blub'}]},
+        {:left=>{"name"=>"bla", "id"=>2}},
+        {:right=>{"name"=>"blub", "id"=>3}}
+      ]
+    ensure
       $stdout = org_stdout
     end
   end
