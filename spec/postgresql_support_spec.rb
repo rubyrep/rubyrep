6647da044f9e0b301ff57ed612dd871cffc64a94
Arndt Lehmann <mail@arndtlehmann.com>
2009-02-16 21:11:23 +0900

    Added spec to verify that timestamps with micro second components are read and written correctly.

---
+++ spec/postgresql_support_spec.rb
@@ -0,0 +1,37 @@
+require File.dirname(__FILE__) + '/spec_helper.rb'
+
+include RR
+
+require File.dirname(__FILE__) + "/../config/test_config.rb"
+
+describe "PostgreSQL schema support" do
+  before(:each) do
+    Initializer.configuration = standard_config
+  end
+
+  after(:each) do
+  end
+
+  if Initializer.configuration.left[:adapter] == 'postgresql'
+
+    it "should read & write microsecond times" do
+      session = nil
+      begin
+        session = Session.new
+        session.left.begin_db_transaction
+        session.left.insert_record('extender_type_check',
+          {'id' => 2, 'timestamp' => Time.local(2009, "feb", 16, 20, 48, 1, 543)}
+        )
+
+        org_cursor = session.left.select_cursor("select id, timestamp from extender_type_check where id = 2")
+        cursor = TypeCastingCursor.new session.left, 'extender_type_check', org_cursor
+
+        row = cursor.next_row
+        row['timestamp'].usec.should == 543
+        cursor.clear
+      ensure
+        session.left.rollback_db_transaction if session
+      end
+    end
+  end
+end
