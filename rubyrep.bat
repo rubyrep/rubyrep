242bf1653ff22a41b610f282f45cb2cb2262e11a
Arndt Lehmann <alehmann@jpn.tuv.com>
2017-05-31 15:49:20 +0900

    The jruby rubyrep scripts should be isolated from other gem instalations

--- rubyrep.bat
+++ rubyrep.bat
@@ -1,4 +1,9 @@
 @echo off
+
+REM ensure isolation from rvm or other possibly existing ruby installations
+set GEM_HOME=
+set GEM_PATH=
+
 set jruby_path=%~dp0jruby\bin\jruby.bat
 set rubyrep_path=%~dp0bin\rubyrep
 %jruby_path% --server %rubyrep_path% %1 %2 %3 %4 %5 %6 %7 %8 %9
